d30ab8f8483d1a5e79b0dd7350647bd8ee97224e
68pisuths-crypto <68pisuth.s@nsru.ac.th>
2026-08-26 12:00:00 +0700

    Add Username and password

--- form.php
+++ form.php
@@ -8,6 +8,8 @@
         NAME: <input type="text" name="member_name"><br>
         TEL: <input type="text" name="member_tel"><br>
         EMAIL: <input type="email" name="member_email"><br>
+        USERNAME: <input type="text" name="member_user"><br>
+        PASSWORD: <input type="password" name="member_pass"><br>
         <input type="submit" name="submit" value="OK">
         <input type="reset" name="reset" value="Cancel">
     </form>
--- result.php
+++ result.php
@@ -8,9 +8,14 @@
         $name = $_POST['member_name'];
         $tel = $_POST['member_tel'];
         $email = $_POST['member_email'];
+        $user = $_POST['member_user'];
+        $pass = $_POST['member_pass'];
+
         echo "ชื่อ (name) = " . htmlspecialchars($name) . "<br>";
         echo "เบอร์โทรศัพท์ (tel) = " . htmlspecialchars($tel) . "<br>";
         echo "อีเมล (email) = " . htmlspecialchars($email) . "<br>";
+        echo "ชื่อผู้ใช้ (username) = " . htmlspecialchars($user) . "<br>";
+        echo "รหัสผ่าน (password) = " . htmlspecialchars($pass) . "<br>";
     }
     ?>
 </body>
