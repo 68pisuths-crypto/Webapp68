f19f85fd23c08ab4dab1218561ef75d9a72d646e
68pisuths-crypto <68pisuth.s@nsru.ac.th>
2026-08-26 12:00:00 +0700

    ADD GENDER

--- form.php
+++ form.php
@@ -10,6 +10,11 @@
         EMAIL: <input type="email" name="member_email"><br>
         USERNAME: <input type="text" name="member_user"><br>
         PASSWORD: <input type="password" name="member_pass"><br>
+                GENDER: 
+        <input type="radio" name="member_gender" value="Male" checked> Male
+        <input type="radio" name="member_gender" value="Female"> Female
+        <input type="radio" name="member_gender" value="Other"> Other<br>
+        
         <input type="submit" name="submit" value="OK">
         <input type="reset" name="reset" value="Cancel">
     </form>
--- result.php
+++ result.php
@@ -10,12 +10,14 @@
         $email = $_POST['member_email'];
         $user = $_POST['member_user'];
         $pass = $_POST['member_pass'];
+        $gender = $_POST['member_gender'];
 
         echo "ชื่อ (name) = " . htmlspecialchars($name) . "<br>";
         echo "เบอร์โทรศัพท์ (tel) = " . htmlspecialchars($tel) . "<br>";
         echo "อีเมล (email) = " . htmlspecialchars($email) . "<br>";
         echo "ชื่อผู้ใช้ (username) = " . htmlspecialchars($user) . "<br>";
         echo "รหัสผ่าน (password) = " . htmlspecialchars($pass) . "<br>";
+        echo "เพศ (gender) = " . htmlspecialchars($gender) . "<br>";
     }
     ?>
 </body>
