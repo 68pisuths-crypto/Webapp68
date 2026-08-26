7afa361a736c9cdcc0c71c4db76f6263542f6262
68pisuths-crypto <68pisuth.s@nsru.ac.th>
2026-08-26 12:00:00 +0700

    add login.php

--- login.php
+++ login.php
@@ -2,6 +2,6 @@
     <form name="form-Login" method="POST" action="check-login.php">
         USERNAME: <input type="text" name="member_username"><br>
         PASSWORD: <input type="password" name="member_password"><br>
-    <input type="submit" value="LOGIN">
+    <input type="submitt" value="LOGIN">
 </form>
 </html>
